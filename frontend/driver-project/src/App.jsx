b0e1380dd9d5fee87294be5540ec5901cefe97ca
Amir-Sharafi-86 <Amirsharafi8899@gmail.com>
2025-08-07 19:52:46 +0330

    install react-hook-form and using in project

--- frontend/driver-project/src/App.jsx
+++ frontend/driver-project/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-
+import { useRoutes } from 'react-router-dom'
+import routes from './routes'
 function App() {
-
+  const route = useRoutes(routes)
   return (
     <>
-     
+     {route}
     </>
   )
 }
---
+++ frontend/driver-project/src/routes.jsx
@@ -0,0 +1,8 @@
+// routes.jsx
+import React from 'react'; 
+import Login from './components/login/login';
+const routes = [
+  { path: '/Login', element: <Login /> },
+];
+
+export default routes;
